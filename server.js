63a797032104d41a0c9dc2c7207a8f028507d802
Viwyn <riaruplays@gmail.com>
2024-09-24 22:27:53 +0800

    changed some port hosting settings

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 import express from "express"
 const app = express()
 
-import fs from "fs";
-import http from "http";
-import https from "https";
-var privateKey  = fs.readFileSync('/etc/letsencrypt/live/viwyn.com/privkey.pem', 'utf8');
-var certificate = fs.readFileSync('/etc/letsencrypt/live/viwyn.com/fullchain.pem', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
-
 //cors permissions
 import cors from 'cors'
 const corsOptions = {
@@ -34,8 +26,6 @@ app.use('/api', apiRouter)
 import {default as loginRouter} from "./routes/login.js";
 app.use('/login', loginRouter)
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(3001, 'localhost');
-httpsServer.listen(3000, 'localhost');
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
